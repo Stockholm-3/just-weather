250507e590566c781ea7510358c19278671a07ff
TheFreeDuck <fredrikthorcewers@gmail.com>
2025-11-03 15:54:39 +0100

    remove useless read_all and write_all in tcp client

--- src/lib/tcp_client.c
+++ src/lib/tcp_client.c
@@ -77,40 +77,6 @@ int tcp_client_read(TCPClient* c, uint8_t* buf, size_t len) {
     return n;
 }
 
-// Writes all data from buf to socket
-int tcp_client_write_all(TCPClient* c, const uint8_t* buf, size_t len) {
-    int total_sent = 0;
-
-    while (total_sent < len) {
-        int n = tcp_client_write(c, buf + total_sent, len - total_sent);
-        if (n < 0) {
-            return -1;
-        }
-        total_sent += n;
-    }
-
-    return total_sent;
-}
-
-// Revives all data that can fit in the give buf
-// if messages is longer that buf the messages will be cut of
-int tcp_client_read_all(TCPClient* c, uint8_t* buf, size_t len) {
-    int total_received = 0;
-
-    while (total_received < len) {
-        int n = tcp_client_read(c, buf + total_received, len - total_received);
-        if (n < 0) {
-            return -1; // error
-        }
-        if (n == 0) {
-            break; // connection closed
-        }
-        total_received += n;
-    }
-
-    return total_received;
-}
-
 void tcp_client_disconnect(TCPClient* c) {
     if (c->fd >= 0) {
         close(c->fd);

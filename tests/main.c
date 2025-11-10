a13a46a1880948c0b7b443c1752edf0a56b07acd
TheRedGuyLmao <theredguy.chas21@gmail.com>
2025-11-10 16:01:04 +0100

    added TCPTests

---
+++ tests/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ifdef UNIT_TESTS
+  #include "tests/tests.h"
+  void parse_string_all(void);
+  void tcp_client_all(void);
+#endif
+
+int main(void) {
+    printf("Test main executed.\n");
+#ifdef UNIT_TESTS
+    RUN_TEST(parse_string_all);
+    RUN_TEST(tcp_client_all);
+#endif
+    return 0;
+}
